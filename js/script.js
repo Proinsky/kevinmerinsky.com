0c29bb547acc2b41da87ce412f65d8a78ce0d3f8
Merinsky <kmerinsky@t1401c-mrc-m01.ua.lan>
2018-01-03 10:07:29 -0500

    Seventh Commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 
+// CARD HEIGHT
+//var cw = $('.t01').width();
+//$('.tile').css({'height':cw})
+
+
 // SCROLL REVEAL 
 window.sr = ScrollReveal();
     sr.reveal('.hero h1', {
@@ -36,6 +41,7 @@ window.sr = ScrollReveal();
 // TODO: SCROLL TO TOP 
 
 // TILE EMPHISIS
+
 // TODO: ADD ANIMATION TO THIS EFFECT, EITHER FADE IN / OUT OR SHAKE
 
 setInterval(function(){emph();}, 3000)
@@ -49,15 +55,43 @@ function emph (){
     $(rand).addClass('tileEmph')
 };
 
+//function emph (){
+//    const tiles = Array('.t01','.t02','.t03','.t04','.t05','.t06','.t07',);
+//    var index = tiles.indexOf(active);
+//    if (index > -1) {
+//    tiles.splice(index, 1);
+//    }
+//    
+//    var rand = tiles[Math.floor(Math.random() * tiles.length)];
+//    var active = rand;
+//    console.log(active);
+//    
+//    $('.tile').removeClass('tileEmph')
+//    $(rand).addClass('tileEmph')
+//};
 
-// TEST ARRAY SUBTRACTION
+//function emphOne (){
+//    var oneTiles = Array('.t01','.t02','.t03',);
+//    
+//    var oneRand = oneTiles[Math.floor(Math.random() * oneTiles.length)];
+//    console.log(oneRand);
+//    
+//    $('.tile').removeClass('tileEmph')
+//    $(oneRand).addClass('tileEmph').delay(3000);
+//};
+//
+//function emphTwo (){
+//    var twoTiles = Array('.t04','.t05','.t06','.t07',);
+//    
+//    var twoRand = twoTiles[Math.floor(Math.random() * twoTiles.length)];
+//    console.log(twoRand);
+//    
+//    $('.tile').removeClass('tileEmph')
+//    $(twoRand).addClass('tileEmph')
+//};
 
-//var list = Array('01','02','03','04');
-//var rand = list[Math.floor(Math.random() * list.length)];
-//
-//var instance = list - rand;
-//
-//console.log(instance);
+
+
 
 
 // TODO: ADD HOVER STATE TO TILES
