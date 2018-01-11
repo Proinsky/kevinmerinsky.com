4d2d6aed31f1dde6ba0887fefa343cd7746f9056
Proinsky <kevinmerinsky@gmail.com>
2018-01-11 08:02:49 -0500

    Commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 
+// CARD HEIGHT
+//var cw = $('.t01').width();
+//$('.tile').css({'height':cw})
+
+
 // SCROLL REVEAL 
 window.sr = ScrollReveal();
     sr.reveal('.hero h1', {
@@ -35,7 +40,10 @@ window.sr = ScrollReveal();
 
 // TODO: SCROLL TO TOP 
 
+
 // TILE EMPHISIS
+
+
 // TODO: ADD ANIMATION TO THIS EFFECT, EITHER FADE IN / OUT OR SHAKE
 
 setInterval(function(){emph();}, 3000)
@@ -48,16 +56,56 @@ function emph (){
     $('.tile').removeClass('tileEmph')
     $(rand).addClass('tileEmph')
 };
-
-
-// TEST ARRAY SUBTRACTION
-
-//var list = Array('01','02','03','04');
-//var rand = list[Math.floor(Math.random() * list.length)];
 //
-//var instance = list - rand;
+//function gif (){
+//    $('.t01').css("background-image", "url(./img/kevinBW)")
+//}
+
+
+//$(".t01").hover(function(){
+//    $(".t01").css("display", "none");
+//    }, function(){
+//    $(".d01").css("display", "inherit");
+//});
+
+
+//function emph (){
+//    const tiles = Array('.t01','.t02','.t03','.t04','.t05','.t06','.t07',);
+//    var index = tiles.indexOf(active);
+//    if (index > -1) {
+//    tiles.splice(index, 1);
+//    }
+//    
+//    var rand = tiles[Math.floor(Math.random() * tiles.length)];
+//    var active = rand;
+//    console.log(active);
+//    
+//    $('.tile').removeClass('tileEmph')
+//    $(rand).addClass('tileEmph')
+//};
+
+//function emphOne (){
+//    var oneTiles = Array('.t01','.t02','.t03',);
+//    
+//    var oneRand = oneTiles[Math.floor(Math.random() * oneTiles.length)];
+//    console.log(oneRand);
+//    
+//    $('.tile').removeClass('tileEmph')
+//    $(oneRand).addClass('tileEmph').delay(3000);
+//};
 //
-//console.log(instance);
+//function emphTwo (){
+//    var twoTiles = Array('.t04','.t05','.t06','.t07',);
+//    
+//    var twoRand = twoTiles[Math.floor(Math.random() * twoTiles.length)];
+//    console.log(twoRand);
+//    
+//    $('.tile').removeClass('tileEmph')
+//    $(twoRand).addClass('tileEmph')
+//};
+
+
+
 
 
 // TODO: ADD HOVER STATE TO TILES
